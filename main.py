1150da09e7654dcf98ef34702d3c88331166ed26
Abramchenko <Alexandre.2005@mail.ru>
2020-02-23 18:00:48 +0300

    upgrade main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import telebot
+import os
 
-with open('./private/token', 'r') as token:
-    TOKEN = token.read()
+
+class TokenException(Exception):
+    pass
+
+
+try:
+    TOKEN = os.environ['BOT_TOKEN']
+except KeyError:
+    try:
+        with open('./private/token', 'r') as tok:
+            TOKEN = tok.read()
+    except FileNotFoundError:
+        raise TokenException("Can't find token file for connection")
 
 # init bot
 tbot = telebot.TeleBot(TOKEN)
+
 
 # Create basic func
 @tbot.message_handler(commands=['start', 'help'])
